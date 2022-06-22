95025ccabe5a72f1fc0987b597a20c7c578a6635
JaberHQ <jaber4hmed@gmail.com>
2022-06-22 13:28:34 +0100

    misc

--- Source/ParkourGame/ParkourGameCharacter.h
+++ Source/ParkourGame/ParkourGameCharacter.h
@@ -58,6 +58,9 @@ protected:
 	/** Handler for when a touch input stops. */
 	void TouchStopped(ETouchIndex::Type FingerIndex, FVector Location);
 
+	void StartSprint();
+	void StopSprint();
+
 protected:
 	// APawn interface
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
